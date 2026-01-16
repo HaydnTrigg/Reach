ee0bb40ae75b7bd184ce976b5255e9b260156211
Haydn Trigg <me@haydntrigg.com>
2026-01-16 12:14:48 +1030

    filesystem skeleton

---
+++ src/interfacelib/load_autosave_queue_film_description_component.cpp
@@ -0,0 +1,16 @@
+/* ---------- headers */
+
+
+/* ---------- constants */
+
+/* ---------- definitions */
+
+/* ---------- prototypes */
+
+/* ---------- globals */
+
+/* ---------- public code */
+
+/* ---------- private code */
+
+/* ---------- reverse engineering */
